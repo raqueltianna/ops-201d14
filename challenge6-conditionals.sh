d95e5f2af09d68f622e8b9777331f68a6c44a94b
Tianna Farrow <147196888+raqueltianna@users.noreply.github.com>
2023-11-09 18:06:16 -0500

    Update challenge6-conditionals.sh

--- challenge6-conditionals.sh
+++ challenge6-conditionals.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Script Name:                        Conditionals.sh
+# Script Name:                        challenge6-conditionals.sh
 # Author:                             Tianna Farrow 
 # Date of Latest Revision:            10/30/2023
 # Purpose:                            Check for files/directories and create them if they do not exist.
-# Execution:                          bash conditionals.sh  or ./conditionals.sh chmod -x conditionals.sh
+# Execution:                          bash challenge6-conditionals.sh  or ./challenge6-conditionals.sh chmod -x challenge6-conditionals.sh
 # Additional resources:               https://github.com/codefellows/seattle-ops-201d14/tree/main/class-06/challenges; https://github.com/codefellows/seattle-ops-201d14/blob/main/class-06/demo/demo.sh
 
 # Decleration of the variables 
